523928dfbba000f3d2e859f75a6a9a973da0e87f
TomisTony <1158176767@qq.com>
2023-04-24 22:52:08 +0800

    完成了Client的部分TODO

--- Client/client.py
+++ Client/client.py
@@ -4,6 +4,7 @@ from kazoo.protocol.states import ZnodeStat, WatchedEvent, EventType
 import requests
 import threading
 import json
+import sql_metadata
 
 class Buffer:
     def __init__(self,zk, node_path, lock) -> None:
@@ -31,31 +32,32 @@ class Buffer:
     
     
     def get_region_url(self, table_name) -> str | None:
-        with self.lock:
+        lock = self.lock if lock == None else lock
+        with lock:
             if table_name in self.table_region_map:
                 region_name = self.table_region_map[table_name]
                 if region_name not in self.region_hosts_map:
                     # 这种情况理应不存在
                     print("WARNING: region found in table_region_map but not found in region_hosts_name.")
-                    print("Buffer will be refreshed")
+                    print("INFO: Buffer will be refreshed")
                     # 将本函数已经获得锁传给refresh_buffer,避免refresh_buffer内部继续请求self.lock造成死锁
                     self.refresh_buffer(self.lock)
                 return self.region_hosts_map[region_name]
-            else: return None 
+            else: return None
     
     def get_region_data(self, region_name):
         try:
             data:bytes = self.zk.get(self.node_path+'/'+region_name)[0]
             return data.decode()
         except Exception as e:
-            print("get_region_data Error", e)
+            print("ERROR: get_region_data Error", e)
     
     # 开启region节点列表监听
     def open_region_list_watcher(self):
         try:
             self.region_list_watcher = self.zk.ChildrenWatch(self.node_path,self.region_list_changed)
         except Exception as e:
-            print("children_list_watcher Error:", e)
+            print("ERROR: children_list_watcher Error:", e)
     
     
     # 开启某个region的监视器
@@ -65,7 +67,7 @@ class Buffer:
             with self.lock:
                 self.region_watchers[region_name] = region_watcher
         except Exception as e:
-            print("region_data_watcher Error:", e)
+            print("ERROR: region_data_watcher Error:", e)
              
              
     # 重置整个buffer    
@@ -86,13 +88,13 @@ class Buffer:
                 try:
                     self.region_list_watcher()
                 except Exception as e:
-                    print("clear region_list_watcher Error", e)
+                    print("ERROR: clear region_list_watcher Error", e)
                     
                 for watch in self.region_watchers:
                     try:
                         watch()
                     except Exception as e:
-                        print("clear region_watcher Error", e)   
+                        print("ERROR: clear region_watcher Error", e)   
                 self.region_list_watcher = None
                 self.region_watchers.clear()
                     
@@ -101,7 +103,7 @@ class Buffer:
             for region_name in self.region_names:
                 self.open_region_data_watcher(region_name)
         except Exception as e:
-            print("refresh_buffer Error",e)
+            print("ERROR: refresh_buffer Error",e)
     
     
     # 在整个缓存中删除有关该region的所有信息
@@ -132,7 +134,7 @@ class Buffer:
             self.table_region_map = {k: v for k, v in self.table_region_map.items() if v != region_name}  
                      
         except Exception as e:
-            print("delete_region Error",e)
+            print("ERROR: delete_region ERROR",e)
     
     
     # WARNING: 该方法并不是线程安全的,请在线程安全的条件下调用该函数
@@ -207,7 +209,7 @@ class Client:
     def __init__(self, zk_hosts):
         self.zk = KazooClient(hosts=zk_hosts)
         # 查看zookeeper状态
-        self.zk.add_listener(lambda state: print('ZooKeeper connection state:', state))
+        self.zk.add_listener(lambda state: print('INFO: ZooKeeper connection state:', state))
         self.zk.start()
         # 新建一个线程锁，保证缓存的安全
         self.lock = threading.Lock()
@@ -215,27 +217,93 @@ class Client:
         
         # Buffer管理类
         self.buffer = Buffer(self.zk,self.node_path, self.lock)
+    
+    
+    # 暂时不支持多表操作，因此只返回解析出的第一张表    
+    def get_table_from_sql(self, sql: str) -> list[str]:
+        return sql_metadata.Parser(sql).tables[0]
+    
+    
+    # 检查sql是否为create命令
+    def is_create_sql(self, sql: str) -> bool:
+        return str(sql_metadata.Parser(sql).tokens[0]).lower() == "create"
+    
+    
+    def get_region_data(self, region_name):
+        try:
+            data:bytes = self.zk.get(self.node_path+'/'+region_name)[0]
+            return data.decode()
+        except Exception as e:
+            print("ERROR: get_region_data Error", e)
+    
+    
+    # 负载均衡,从zk中查询并返回表最少的region的url
+    def get_least_table_region_url(self) -> str:
+        try: 
+            # 从zk获得region的信息并统计
+            regions: list[str] = self.zk.get_children(self.node_path)
+            region_data_map: dict[str, int] = dict()
+            for region in regions:
+                data = self.get_region_data(region)
+                region_data_map[region] = data.split(",")
+            # 返回最长（即包含table最多）的region的url
+            max_region = max(region_data_map, key=lambda k: len(region_data_map[k]))
+            # 返回 hosts:port
+            return region_data_map[0]+":"+region_data_map[2]        
+        except Exception as e:
+            print("ERROR: get_least_table_region_url Error",e)
         
-    # TODO: 使用sql解析库来获取table_name/查询各个region的负载状况并做负载均衡/正常拿到response的处理    
-    def exec(sql):
-        url = Buffer.get_region_url()
+        
+    # TODO: 格式化输出select    
+    def exec(self, sql):
+        if self.is_create_sql(sql): # create命令,需要根据负载均衡找到所需的region_url
+            url = self.get_least_table_region() 
+        else:   # 非create命令,查找对应的table所在的region
+            table_name = self.get_table_from_sql(sql)
+            url = Buffer.get_region_url(table_name)
+            # 检查Buffer内是否含有该table
+            if url == None:
+                print("INFO: table_name not found in buffer!")
+                print("INFO: Buffer will be refreshed!")
+                self.buffer.refresh_buffer()
+                url = Buffer.get_region_url(table_name)
+                if url == None:
+                    print("WARNING: table_name not found in refreshed buffer!")
+                    print("WARNING: Please check your sql!")
+                    return
+                
+        # 请求region超时后的最大尝试次数
         try_times = 3
+        response: requests.Response = None
         while try_times != 0:
             try:
-                response = requests.post(url, data = json.dumps({"sql":sql}), timeout=4)
+                response: requests.Response = requests.post(url, data = json.dumps({"sql":sql}), timeout=4)
                 break
             except requests.exceptions.Timeout:
                 try_times = try_times - 1
-                print("sql exec timeout, remain try: ", try_times)
+                print("INFO: sql exec timeout, remain try: ", try_times)
         if try_times == 0:
             print("Error: Max try time!")
             return None
         # 正常拿到了response
-        pass
+        if response.ok():
+            result = response.json()
+            if result["statusCode"].lower() != "success":
+                print("WARNING: sql exec failed!")
+                print(result["msg"])
+            else:
+                if result["type"].lower() != "select":
+                    print("Info: sql exec success!")
+                    print(result["msg"])
+                else:
+                    # 格式化输出select
+                    pass
+        else:
+            print("WARNING: response not OK!")
+            print(response.json())
 
     def close(self):
         self.zk.close()
-
 
 def print_help():
     print("\n----------Help---------------")
@@ -250,7 +318,7 @@ if __name__ == '__main__':
     print("Welcome!\n")
     print_help()
     while True:
-        cmd = input("cmd")
+        cmd = input("\ncmd:")
         if cmd.lower() == 'h' or cmd.lower() == 'help':
             print_help()
         elif cmd.lower() == 'q':
